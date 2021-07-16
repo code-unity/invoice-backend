5989f772a228b34d4d0f08c12aca1058ef299f9e
Dheerajpenta <2018chb1053@iitrpr.ac.in>
2021-07-16 12:44:37 +0530

    initialized express and added some dev-dep and dep

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};

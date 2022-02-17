585431b37e0765bdf7a398237004dea95bcc1227
harshith9999 <harshiith.p@gmail.com>
2022-02-17 13:43:36 +0530

    small changes in timesheet crud

--- routes/timesheet.js
+++ routes/timesheet.js
@@ -11,12 +11,12 @@ router.post("/", async (req, res) => {
       const datas = {
         _id: new mongoose.Types.ObjectId(),
         description: requests[i].description,
-        no_of_hours: parseInt(requests[i].noOfHours),
+        no_of_hours: requests[i].no_of_hours,
         attendance: requests[i].attendance,
-        date: requests[i].selectedDate,
+        date: requests[i].date,
       };
       const data = new timesheet(datas);
-      const newTimeSheet = await data.save();
+      await data.save();
     }
     res.status(200).json({
       status: {
@@ -36,5 +36,58 @@ router.post("/", async (req, res) => {
     console.log("error", err);
   }
 });
+router.get("/", async (req, res) => {
+  try {
+    const timeSheet = await timesheet.find({})
+    res.status(200).json({
+      status: {
+        success: true,
+        code: 200,
+        message: constants.SUCCESSFUL,
+        data: timeSheet
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: {
+        success: false,
+        code: 400,
+        message: err.message,
+      },
+    });
+    console.log("error", err);
+  }
+});
 
+router.put("/", async (req, res) => {
+  let requests = req.body;
+  console.log(requests)
+  try {
+    for (let i = 0; i < requests.length; i++) {
+  await timesheet.findByIdAndUpdate(requests[i]._id,{
+    _id: requests[i]._id,
+    description: requests[i].description,
+    no_of_hours: requests[i].no_of_hours,
+    attendance: requests[i].attendance,
+    date: requests[i].date,
+  });
+    }
+    res.status(200).json({
+      status: {
+        success: true,
+        code: 204,
+        message: constants.SUCCESSFUL,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: {
+        success: false,
+        code: 400,
+        message: err.message,
+      },
+    });
+    console.log("error", err);
+  }
+});
 module.exports = router;

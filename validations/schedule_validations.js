61302d82ae0e8bd1e98f8e6fd630ba4316853312
satish chandra <2019meb1253@iitrpr.ac.in>
2022-07-13 22:04:25 +0530

    updated backend repo files related to schedule invoice task

--- validations/schedule_validations.js
+++ validations/schedule_validations.js
@@ -1,28 +1,25 @@
-//Dependencies Imported :
 const { check } = require("express-validator");
-
-
-//Express Validations for client :
 module.exports = function schedule_validator() {
 
     return [
-        //Validation for client :
-        check("client")
-            .notEmpty().withMessage("Please enter client name").bail(),
+        check("isDisabled")
+            .notEmpty().withMessage("Please enter disabled status").bail(),
 
-        //Validation for invoice number :
+        check("scheduleName")
+            .notEmpty().withMessage("Please enter schedule name").bail(),
+
+        check("clientId")
+            .notEmpty().withMessage("Please enter client").bail(),
+
         check("invoiceNumber")
             .notEmpty().withMessage("Please enter invoice number").bail(),
 
-        //Validation for date :
         check("date")
             .notEmpty().withMessage("Please enter date").bail(),
 
-        //Validation for frequency :
         check("frequency")
             .notEmpty().withMessage("Please enter frequency").bail(),
 
-        //Validation for time :
         check("time")
             .notEmpty().withMessage("Please enter time").bail(),
     ];

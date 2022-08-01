167f120232f4ce0ea84da4c27dc2ece4f9005654
Manoj290501 <chunduri.saimanoj@gmail.com>
2022-08-01 20:19:29 +0530

    Revert "Revert "changes""
    This reverts commit 58a19acffe8fa5b6294c3935152caca84707420f.

--- app.js
+++ app.js
@@ -15,19 +15,17 @@ var cors = require("cors");
 //Importing Config File :
 var config = require("./config/config.json");
 
+
 //MongoDb Connection :
-mongoose.connect(
-  config.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-  function (err, conn) {
-    if (err) {
-      console.log("mongodb connection error", err);
+mongoose.connect(config.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true},function(err, conn){
+    if(err){
+        console.log("mongodb connection error", err);
     }
-    if (!err && conn) {
-      console.log("mongodb connection established");
+    if(!err && conn){
+        console.log("mongodb connection stablished");
     }
-  }
-);
+});
+
 
 
 //Routes Imported :
@@ -40,16 +38,14 @@ var payslipRouter = require("./routes/payslip");
 
 //Express Application :
 var app = express();
-var scheduleRouter = require("./routes/schedule");
-var invoiceFilterRouter = require("./routes/invoiceFilter");
-var usersRouter = require("./routes/users");
 
-//Express Application :
-var app = express();
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+
 
 app.use(cors());
 app.use(logger("dev"));
@@ -58,32 +54,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+
+
 //API Routes :
 app.use("/admin", adminRouter);
 app.use("/client", clientRouter);
 app.use("/invoice", invoiceRouter);
-app.use("/timesheet", timeSheetRouter);
+app.use("/timesheet",timeSheetRouter)
 app.use("/candidate", candidateRouter);
-app.use("/invoiceFilter", invoiceFilterRouter.router);
-app.use("/payslip", payslipRouter);
-app.use("/schedule", scheduleRouter);
-app.use("/users", usersRouter);
+app.use("/payslip",payslipRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
+
+
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
-module.exports = app;
 
+
+module.exports = app;

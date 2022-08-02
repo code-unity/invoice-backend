1ee141d99203fc3516dde2208a78da6417194cbd
Manoj290501 <chunduri.saimanoj@gmail.com>
2022-08-02 09:15:24 +0530

    initial commit

--- models/candidate.js
+++ models/candidate.js
@@ -15,8 +15,8 @@ var candidateSchema = mongoose.Schema({
     //Validation for Email :
     email: {
         type: String,
-        unique: [true, "email already exists"],
-        required: [true, "please enter email address"],
+        required: false,
+        unique: false,
         lowercase: [true, "email address must be lowercase"],
         validate: [
             {
@@ -27,6 +27,23 @@ var candidateSchema = mongoose.Schema({
             }
         ]
     },
+
+    //Validation for Pan No :
+    pan_no: {
+        type: String,
+        unique: [true, "Pancard already exists"],
+        required: [true, "Please enter Pan No"],
+        validate: [
+            {
+                validator: function(v) {
+                    return  /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/.test(v);
+                },
+                Error: "please enter a valid email address"
+            }
+        ]
+
+    },
+    
     //Validation for Date of Birth :
     date_of_birth: {
         type: String,
@@ -46,7 +63,7 @@ var candidateSchema = mongoose.Schema({
     //Validation for payment terms :
     payment_terms: {
         type: String,
-        required: [true, "please enter payment terms"]
+        required: false
     },
     role: {
         type: String,
@@ -56,6 +73,92 @@ var candidateSchema = mongoose.Schema({
         type: Boolean,
         default:true
     },
+     //Validation for Basic :
+     Basic: {
+        type: String,
+        required: [true, "please enter Basic Salary"]
+    },
+
+    //Validation for DA :
+    D_allow: {
+        type: String,
+        required: [true, "please enter DA"]    },
+
+    //Validation for HR_allow :
+    HR_allow: {
+        type: String,
+        required: [true, "please enter HR allowance"]
+    },
+
+    //Validation for Bonus :
+    Bonus: {
+        type: String,
+        required: [true, "Please Enter Bonus"]
+    },
+
+    //Validation for conveyance :
+    conveyance: {
+        type: String,
+        required: [true, "Please Enter conveyance"]
+    },
+
+    //Validation for others :
+    others: {
+        type: String,
+        required: [true, "Please enter others Allowance"]
+    },
+
+    //Validation for total_earnings :
+    total_earnings: {
+        type: String,
+        required: [true, "Please enter total_earnings"]
+    },
+
+    //Validation for prof_tax :
+    prof_tax: {
+        type: String,
+        required: [true, "Please enter Prof. Tax"]
+    },
+
+    //Validation for p_f_employee :
+    p_f_employee: {
+        type: String,
+        required: [true, "Please enter Employee PF"]
+    },
+
+    //Validation for p_f_employer :
+    p_f_employer: {
+        type: String,
+        required: [true, "Please enter Employer PF"]
+    },
+    
+
+    //Validation for td_S :
+    td_S: {
+        type: String,
+        required: [true, "Please enter TDS"]
+    },
+
+    //Validation for other_tax :
+    other_tax: {
+        type: String,
+        required: [true, "Please enter other_tax"]
+    },
+
+    
+    //Validation for Designation :
+    Designation: {
+        type: String,
+        required: [true, "Please enter Designation"]
+    },
+
+    //Validation for Designation :
+    type: {
+        type: String,
+        required: [true, "Please enter Type of employee"]
+    },
+
+
    
 });
 
